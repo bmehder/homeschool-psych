15e11dd7c8807b5ccfe99493547c0ea2cb84ac12
bmehder <bmehder@gmail.com>
2024-05-27 18:35:42 +0200

    seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+  <title>Homeschool Psychology – Tim Rice, D.Min, LPC</title>
+  <meta name="description" content="Help high school students understand that
+		when it comes to psychology, the Christian worldview makes sense.">
+</svelte:head>
+
 <h1>Welcome to Homeschool Psychology</h1>
 
 <section>
